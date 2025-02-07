53dcd1448f222eaa58c1c0e7f032416526f6f9ed
MoonGwangHee <mgh7020@gmail.com>
2025-02-07 16:02:12 +0900

    feat : 6. 주문 도메인 개발 프로세스 中, 테스트 코드 작성

---
+++ src/test/java/inflearn/jpa/OrderServiceTest.java
@@ -0,0 +1,54 @@
+package inflearn.jpa;
+
+
+import inflearn.jpa.domain.Member;
+import inflearn.jpa.domain.Order;
+import inflearn.jpa.domain.OrderStatus;
+import inflearn.jpa.domain.item.Item;
+import inflearn.jpa.repository.OrderRepository;
+import inflearn.jpa.service.OrderService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+
+    @Test
+    public void 상품주문() throws Exception {
+        
+        // Given
+        Member member = createMember();
+        Item item = createBook("시골 JPA", 10000, 10);    // 이름 , 가격 , 재고
+        int orderCount = 2;
+
+        // When
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+
+        // Then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals("상품 주문시 상태는 ORDER", OrderStatus.ORDER, getOrder.getStatus());
+        assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
+        assertEquals("주문 가격은 가격 * 수량 이다.", 10000 * 2, getOrder.getTotalPrice());
+        assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, item.getStockQuantity());
+
+
+    }
+}
